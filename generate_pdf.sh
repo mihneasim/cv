39dff400d3eecc20abeb9f7bedc41e6c18fd28cb
Mihnea Simian <mihnea.simian@ing.ro>
2016-10-20 12:40:12 +0300

    open document after successful generation

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rst2pdf --use-floating-images --default-dpi=100 README.rst -o "Mihnea Simian.pdf" \
-        --stylesheets=static/style.json
+        --stylesheets=static/style.json && open 'Mihnea Simian.pdf'
 
